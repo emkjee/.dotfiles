5dc46ddf09d995686920853da4504f52c75fcf58
emkjee <123936268+emkjee@users.noreply.github.com>
2023-06-06 18:16:09 +0530

    vscode setup file [initial commit]

---
+++ 2.setup_vscode.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+echo "\nbot: create vscode setting symlink!"
+ln -f ~/Dotfiles/code_settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+echo "\nbot: start installing vscode extentions!\n"
+
+code --install-extension albert.TabOut --force
+code --install-extension ms-python.python --force
+code --install-extension vscode-icons-team.vscode-icons --force
+code --install-extension sdras.night-owl --force
+code --install-extension GrapeCity.gc-excelviewer --force
+
+echo "\nbot: finished installing vscode extentions!\n"
