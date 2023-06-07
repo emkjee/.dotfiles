867e97826a695262636896d683afae76c2eda7cd
emkjee <123936268+emkjee@users.noreply.github.com>
2023-06-07 19:04:49 +0530

    Fixed output messages to reflect the correct files

--- 0.setup_symlinks.zsh
+++ 0.setup_symlinks.zsh
@@ -1,9 +1,9 @@
 
 # create symlinks
 echo "\nbot: start creating symlinks!"
-ln -sf ~/Dotfiles/zshenv ~/.zshenv; echo ".zshenv created"
-ln -sf ~/Dotfiles/zshrc ~/.zshrc; echo ".zshrc created"
-ln -sf ~/Dotfiles/gitconfig ~/.gitconfig; echo ".gitconfig created"
+ln -sf ~/Dotfiles/zshenv ~/.zshenv; echo "~/.zshenv created"
+ln -sf ~/Dotfiles/zshrc ~/.zshrc; echo "~/.zshrc created"
+ln -sf ~/Dotfiles/gitconfig ~/.gitconfig; echo "~/.gitconfig created"
 echo "bot: finished creating symlinks!\n"
 
 #reload zsh to reflect changes
