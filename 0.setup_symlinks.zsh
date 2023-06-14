891903d50c26516623cb99949782368b139c63d3
emkjee <123936268+emkjee@users.noreply.github.com>
2023-06-14 19:05:45 +0530

    suffix date to backup file names.

--- 0.setup_symlinks.zsh
+++ 0.setup_symlinks.zsh
@@ -3,22 +3,22 @@
 echo "\nbot: start creating symlinks!"
 
 if [[ -f "$HOME/.zshenv" ]]; then
-    cp $HOME/.zshenv $HOME/.zshenv.bkup
+    cp $HOME/.zshenv $HOME/.zshenv.bkup-$(date +%F)
 fi
 ln -sf $HOME/Dotfiles/zshenv $HOME/.zshenv; echo "$HOME/.zshenv created"
 
 if [[ -f "$HOME/.zshrc" ]]; then
-    cp $HOME/.zshrc $HOME/.zshrc.bkup
+    cp $HOME/.zshrc $HOME/.zshrc.bkup-$(date +%F)
 fi
 ln -sf $HOME/Dotfiles/zshrc $HOME/.zshrc; echo "$HOME/.zshrc created"
 
 if [[ -f "$HOME/.gitconfig" ]]; then
-    cp $HOME/.gitconfig $HOME/.gitconfig.bkup
+    cp $HOME/.gitconfig $HOME/.gitconfig.bkup-$(date +%F)
 fi
 ln -sf $HOME/Dotfiles/gitconfig $HOME/.gitconfig; echo "$HOME/.gitconfig created"
 
 if [[ -f "$HOME/.config/helix/config.toml" ]]; then
-    cp $HOME/.config/helix/config.toml $HOME/.config/helix/config.toml.bkup
+    cp $HOME/.config/helix/config.toml $HOME/.config/helix/config.toml.bkup-$(date +%F)
 fi
 ln -sf $HOME/Dotfiles/helix_config.toml $HOME/.config/helix/config.toml; echo "$HOME/.config/helix/config.toml created"
 
