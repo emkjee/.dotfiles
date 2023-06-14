51f594f5b43cede7c4f3b6b0e90aa03f7be48328
emkjee <123936268+emkjee@users.noreply.github.com>
2023-06-14 18:23:25 +0530

    ensure backup of target files before replacing. Also replace ~ with $HOME

--- 0.setup_symlinks.zsh
+++ 0.setup_symlinks.zsh
@@ -1,9 +1,27 @@
 
 # create symlinks
 echo "\nbot: start creating symlinks!"
-ln -sf ~/Dotfiles/zshenv ~/.zshenv; echo "~/.zshenv created"
-ln -sf ~/Dotfiles/zshrc ~/.zshrc; echo "~/.zshrc created"
-ln -sf ~/Dotfiles/gitconfig ~/.gitconfig; echo "~/.gitconfig created"
+
+if [[ -f "$HOME/.zshenv" ]]; then
+    cp $HOME/.zshenv $HOME/.zshenv.bkup
+fi
+ln -sf $HOME/Dotfiles/zshenv $HOME/.zshenv; echo "$HOME/.zshenv created"
+
+if [[ -f "$HOME/.zshrc" ]]; then
+    cp $HOME/.zshrc $HOME/.zshrc.bkup
+fi
+ln -sf $HOME/Dotfiles/zshrc $HOME/.zshrc; echo "$HOME/.zshrc created"
+
+if [[ -f "$HOME/.gitconfig" ]]; then
+    cp $HOME/.gitconfig $HOME/.gitconfig.bkup
+fi
+ln -sf $HOME/Dotfiles/gitconfig $HOME/.gitconfig; echo "$HOME/.gitconfig created"
+
+if [[ -f "$HOME/.config/helix/config.toml" ]]; then
+    cp $HOME/.config/helix/config.toml $HOME/.config/helix/config.toml.bkup
+fi
+ln -sf $HOME/Dotfiles/helix_config.toml $HOME/.config/helix/config.toml; echo "$HOME/helix config created"
+
 echo "bot: finished creating symlinks!\n"
 
 #reload zsh to reflect changes
