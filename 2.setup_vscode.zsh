4ecc99918bb211b59a397831627f1d2278f9084a
emkjee <123936268+emkjee@users.noreply.github.com>
2023-06-19 20:43:56 +0530

    1. Added keybindings symlink. 2. Now take backup before replacing. 3. installed some more extensions.

--- 2.setup_vscode.zsh
+++ 2.setup_vscode.zsh
@@ -1,7 +1,16 @@
 #!/usr/bin/env zsh
 
-echo "\nbot: create vscode setting symlink!"
+if [[ -f ~/Library/Application\ Support/Code/User/settings.json ]]; then
+    cp ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json.bkup-$(date +%F)
+fi
+
+if [[ -f ~/Library/Application\ Support/Code/User/keybindings.json ]]; then
+    cp ~/Library/Application\ Support/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json.bkup-$(date +%F)
+fi
+
+echo "\nbot: create vscode setting & keybindings symlink!"
 ln -sf ~/Dotfiles/code_settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/Dotfiles/code_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 echo "\nbot: start installing vscode extentions!\n"
 
@@ -13,5 +22,10 @@ code --install-extension GrapeCity.gc-excelviewer --force
 code --install-extension mhutchie.git-graph --force
 code --install-extension ex-codes.pine-script-syntax-highlighter --force
 code --install-extension rust-lang.rust-analyzer --force
+code --install-extension dsznajder.es7-react-js-snippets --force
+code --install-extension ritwickdey.LiveServer --force
+code --install-extension ms-vscode-remote.remote-ssh --force
+code --install-extension miguelsolorio.fluent-icons --force
+code --install-extension ms-python.black-formatter --force
 
 echo "\nbot: finished installing vscode extentions!\n"
